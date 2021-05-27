007baee93f4f1528fa0076944d4a01b02cc8cf68
tsuruta <yasu.tsuru68@gmail.com>
2021-05-27 13:14:08 +0900

    get grsim value

--- poe.py
+++ poe.py
@@ -2,7 +2,18 @@
 # coding: UTF-8
 import get_ball_env
 import time
+import threading
+import simple_receiver
 env = get_ball_env.get_ball_env()
+rcv = simple_receiver.receiver()
+robot = rcv.robot
+ball = rcv.ball
+thread = threading.Thread(target=rcv.receive)
+thread.start()
 while True:
     time.sleep(3)
     env.reset()
+    print("ooooooooooooooooooooooooooooooooooooooooooooooooooooo")
+    print(robot.id)
+    print(robot.x, robot.y,  robot.theta)
+    print(ball.x,  ball.y)
---
+++ simple_receiver.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import  socket
+from proto import ssl_vision_wrapper_pb2
+
+class receiver():
+    def __init__(self):
+        multicast_if_addr='192.168.11.15'  
+
+        multicast_group='224.5.23.2'
+        multicast_port=10006
+
+        my_addr='0.0.0.0'
+        server_address=(my_addr, multicast_port)
+
+        self.sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(server_address)
+
+        mreq=socket.inet_aton(multicast_group)+socket.inet_aton(multicast_if_addr)
+        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+        self.robot = robot()
+        self.ball = ball()
+
+    def receive(self):
+        while True:
+            buf = ""
+            # 複数のカメラからデータを受け取るため、whileループで全て受け取る
+            while buf is not False:
+                buf = self.sock.recv(2048)
+                if buf:
+                    # do something
+                    packet = ssl_vision_wrapper_pb2.SSL_WrapperPacket()
+                    packet.ParseFromString(buf)
+
+                    if packet.HasField('detection'):
+                         # ball
+                        for ball in packet.detection.balls:
+                            self.ball.x = ball.x
+                            self.ball.y = ball.y
+                        # yellow robot
+                        for robot in packet.detection.robots_yellow:
+                            if robot.robot_id == 7:
+                                self.robot.id =robot.robot_id
+                                self.robot.x = robot.x
+                                self.robot.y = robot.y
+                                self.robot.theta = robot.orientation
+
+class robot():
+    def __init__(self):
+        self.id = 0
+        self.x = 0.0
+        self.y = 0.0
+        self.theta = 0.0
+
+class ball():
+    def __init__(self):
+        self.x = 0.0
+        self.y = 0.0
