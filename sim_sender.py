f74dc6b31be5e1f5211ed58781165c9b58732794
tsuruta <yasu.tsuru68@gmail.com>
2021-05-27 17:47:42 +0900

    送るコマンドをロボット速度以外固定値にしてみた

--- sim_sender.py
+++ sim_sender.py
@@ -3,7 +3,7 @@
 import socket
 import math
 import time
-import datetime
+import numpy as np
 
 from proto import grSim_Packet_pb2
 
@@ -19,33 +19,27 @@ class SimSender(object):
 
     def send_commands(self, vel):
         packet = grSim_Packet_pb2.grSim_Packet()
-        packet.commands.timestamp = datetime.time()
+        packet.commands.timestamp = time.time()
         packet.commands.isteamyellow = True
 
         packet_command = packet.commands.robot_commands.add()
 
         # ロボットID
-        packet_command.id = command.robot_id
+        packet_command.id = 7
 
         # 走行速度
-        packet_command.veltangent = command.vel_surge if not math.isnan(command.vel_surge) else 0
-        packet_command.velnormal = command.vel_sway if not math.isnan(command.vel_sway) else 0
-        packet_command.velangular = command.vel_angular if not math.isnan(command.vel_angular) else 0
+        packet_command.veltangent = vel[0] * 3
+        packet_command.velnormal = vel[1] * 3
+        packet_command.velangular = vel[2] * 4 * np.pi
 
         # キック速度
-        packet_command.kickspeedx = command.kick_power * self._MAX_KICK_SPEED
+        packet_command.kickspeedx =  0
 
         # チップキック
-        if command.chip_enable:
-            packet_command.kickspeedz = packet_command.kickspeedx
-        else:
-            packet_command.kickspeedz = 0
+        packet_command.kickspeedz = 0
 
         # ドリブラー
-        if command.dribble_power > 0:
-            packet_command.spinner = True
-        else:
-            packet_command.spinner = False
+        packet_command.spinner = False
 
         # タイヤ個別に速度設定しない
         packet_command.wheelsspeed = False
